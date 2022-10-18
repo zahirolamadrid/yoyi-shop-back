c112e452bc224fc4b2a23e5fa4f0eb945b407832
Zahiro <zahiro.lamadrid@creditas.com>
2022-10-18 11:28:19 -0500

    First commit

---
+++ src/main/kotlin/com/yoyi/shop/YoyiShopApplication.kt
@@ -0,0 +1,11 @@
+package com.yoyi.shop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YoyiShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<YoyiShopApplication>(*args)
+}
---
+++ src/test/kotlin/com/yoyi/shop/YoyiShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yoyi.shop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YoyiShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
